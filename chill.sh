a664c572404e95551f4d32b941a87a785f7e4879
Ben Costerton <bencosterton@gmail.com>
2022-03-11 14:08:00 +0000

    Update chill.sh

--- chill.sh
+++ chill.sh
@@ -11,15 +11,3 @@ else
     mpv https://www.youtube.com/watch?v=5qap5aO4i9A --no-video >> /dev/null
 
 fi
-
-
-bold=$(tput bold)
-underline=$(tput smul)
-italic=$(tput sitm)
-info=$(tput setaf 2)
-error=$(tput setaf 160)
-warn=$(tput setaf 214)
-reset=$(tput sgr0)
-echo "${info}Now Chill${reset}
-#echo "${error}ERROR${reset}: This is an ${underline}error${reset} message"
-#echo "${warn}WARN${reset}: This is a ${italic}warning${reset} message"
